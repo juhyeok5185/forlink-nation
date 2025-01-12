0393bc6835532f9c6992acc5009e8ea8fd641b72
danny <qkrwngur5185@naver.com>
2025-01-12 10:35:00 +0900

    added

---
+++ src/main/java/com/danny/forlinkbackendspringboot/ForlinkBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.danny.forlinkbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForlinkBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForlinkBackendSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danny/forlinkbackendspringboot/ForlinkBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.forlinkbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForlinkBackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
