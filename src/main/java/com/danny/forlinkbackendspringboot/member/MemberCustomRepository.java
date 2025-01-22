96abb009861d890eec16c80cfa66cf5e06f3226c
juhyeok5185 <qkrwngur5185@naver.com>
2025-01-22 10:10:26 +0900

    added

--- src/main/java/com/danny/forlinkbackendspringboot/member/MemberCustomRepository.java
+++
@@ -1,5 +0,0 @@
-package com.danny.forlinkbackendspringboot.member;
-
-
-public interface MemberCustomRepository {
-}
--- src/main/java/com/danny/forlinkbackendspringboot/member/MemberRole.java
+++
@@ -1,8 +0,0 @@
-package com.danny.forlinkbackendspringboot.member;
-
-public enum MemberRole {
-    USER,  // 일반 사용자
-    ADMIN; // 관리자
-
-
-}
